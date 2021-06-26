cf164b87cb17455a566d7125351a7590d96f4fe6
Selim Ozel <sozel@wpi.edu>
2021-06-26 16:01:31 +0200

    Python added.

--- source/app.d
+++ source/app.d
@@ -2,5 +2,5 @@ import std.stdio;
 
 void main()
 {
-	writeln("Sequential Least Squares Programming optimization");
+	writeln("Sequential Least Squares Programming optimization - slsqp DPort entry point");
 }
---
+++ source/app.py
@@ -0,0 +1,9 @@
+from scipy.optimize import minimize
+
+
+def main():
+	print("slsqp python entry point")
+
+
+if __name__ == "__main__":
+    main()
